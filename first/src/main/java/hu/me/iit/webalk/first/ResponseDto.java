537014ff3ce5466aa8d5f9d3e94867ceaf71f0cd
jbalazs97 <36412827+jbalazs97@users.noreply.github.com>
2021-09-27 11:38:57 +0200

    first

---
+++ first/src/main/java/hu/me/iit/webalk/first/ResponseDto.java
@@ -0,0 +1,23 @@
+package hu.me.iit.webalk.first;
+
+public class ResponseDto {
+
+	int id;
+	String messeage;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMesseage() {
+		return messeage;
+	}
+	public void setMesseage(String messeage) {
+		this.messeage = messeage;
+	}
+	
+	
+}
